68fa2a3dcfe5be204fc66e90402f362346f45315
josiehpersson <josephine.h.persson@outlook.com>
2020-09-02 09:56:18 +0200

    controllers

--- actorsController.js
+++ actorsController.js
@@ -5,10 +5,13 @@ const sql = require('mssql');
 //database provider reuses a global instance of mssql every time it's added as a req.
 const config = require('./config');
 
-addressesController = () => {
+actorsController = () => {
     //adding controllers get-function
 get = async (req, res) => {
     try {
+        let query = req.params.Id > 0 ? `EXEC GetActor ${req.params.Id}` : 'EXEC GetActors';
+        //skapar en förfrågan som kollar om vi lagt med ett ID i förfrågan eller ej
+        //Om ID är angett så "hämta actor med det ID:et" annars "hämta alla actors"
     }
     catch (err) {
     return res.status(404);
