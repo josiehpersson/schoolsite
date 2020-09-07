93fcca5b5002ce9a27b77e780299c78be01acdab
josiehpersson <josephine.h.persson@outlook.com>
2020-09-07 13:00:10 +0200

    Update teacher.ts

--- KidzSchool/UI/app/teacher.ts
+++ KidzSchool/UI/app/teacher.ts
@@ -5,17 +5,23 @@ export class Teacher {
     prefix:string;
 
     constructor(teacher:any) {
-        this.id = teacher.Id;
-        this.firstName = teacher.FirstName;
-        this.lastName = teacher.LastName;
-        this.prefix = teacher.Prefix;
+        this.id = teacher.id;
+        this.firstName = teacher.firstName;
+        this.lastName = teacher.lastName;
+        this.prefix = teacher.prefix;
     }
 
     public html() {
         return `
-        <div>
-        <p>${this.prefix}. ${this.firstName} ${this.lastName}</p>
-        </div>
+        <div class="teacher-container">
+        <img src="img/teacher2.jpg" class="teacher-picture" alt="teacher2"/>
+        <p class="teacher-info">${this.prefix}. ${this.firstName} ${this.lastName}</p>
+      </div>
+      <div class="teacher-container">
+        <img src="img/teacher3.jpg" class="teacher-picture" alt="teacher3"/>
+        <p class="teacher-info">Teacher three</p>
+      </div>
+    </div>
         `
     }
 }
