0e1bf150d6eca44374501eefc80b5f30a5f87dd3
josiehpersson <josephine.h.persson@outlook.com>
2020-09-07 13:45:02 +0200

    sisra

---
+++ KidzSchool/UI/app/html.ts
@@ -0,0 +1,20 @@
+import {Teacher} from './teacher';
+import {SchoolClass} from './schoolClass';
+
+export const displayTeachers = (teachers:Array<Teacher>):void => {
+    const teachersElement = document.getElementById('teacher-container');
+    if(teachersElement !== null) {
+        teachers.forEach((a:Teacher, idx:number) => {
+            teachersElement.innerHTML += a.html();
+        });
+    }
+}
+
+export const displayClasses = (classes:Array<SchoolClass>):void => {
+    const classesElement = document.getElementById('classes-container');
+    if(classesElement !== null) {
+        classes.forEach((a:SchoolClass, idx:number ) => {
+            classesElement.innerHTML += a.html();
+        });
+    }
+}
