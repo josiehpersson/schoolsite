3dfe5c9b37e19049aa7d2d7b2fcf928a4a691e9b
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-09-07 13:36:44 +0200

    Update app.ts

---
+++ KidzSchool/UI/app/app.ts
@@ -0,0 +1,16 @@
+const main = async () => {
+    try {
+            const api = await import('./api');
+            const teacher = await api.getTeachers();
+            const schoolClass = await api.getClasses();
+            console.log(teacher, schoolClass);
+            
+            const html = await import('./html');
+            html.displayTeachers(teacher.teachers);
+            html.displayClasses(schoolClass.classes);
+        } 
+    catch (e) {
+        console.log(e);
+    }
+}
+main();
